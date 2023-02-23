693583682d0dc7d97c650f3cf222a1f6ec5161c2
MAUREEN-OUGODEV <ougomaureenakinyi@gmail.com>
2023-02-23 19:55:06 +0300

    my first commit of the Calculator

---
+++ main.js
@@ -0,0 +1,18 @@
+var addition = function(number1,number2){
+  return number1 + number2
+}
+var subtraction = function(number3,number4){
+  return number3-number4
+}
+var multiplication = function(number5,number6){
+  return number5 *number6
+}
+var division = function(number7,number8){
+  return number7/number8
+}
+var number5 =  parseInt(prompt("Enter Number:"));
+var number6 =  parseInt(prompt("Enter another nummber:"));
+var results =  parseInt(multiplication(number5,number6));
+alert(results)
+
+
