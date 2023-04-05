ddeb29843e1b200b5f3a2b095ef4e77e15272527
MAUREEN-OUGODEV <ougomaureenakinyi@gmail.com>
2023-04-05 15:07:16 +0300

    pushing togithub

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-var addition = function(number1,number2){
-  return number1 + number2
+var addition = function(number,number){
+  return number+ number
 }
-var subtraction = function(number3,number4){
-  return number3-number4
+var subtraction = function(number,number){
+  return number-number
 }
-var multiplication = function(number5,number6){
-  return number5 *number6
+var multiplication = function(number,number){
+  return number * number
 }
-var division = function(number7,number8){
-  return number7/number8
+var division = function(number,number){
+  return number/number
 }
-var number5 =  parseInt(prompt("Enter Number:"));
-var number6 =  parseInt(prompt("Enter another nummber:"));
-var results =  parseInt(multiplication(number5,number6));
+var number =  parseInt(prompt("Enter Number:"));
+var number =  parseInt(prompt("Enter another nummber:"));
+var results =  parseInt(multiplication(number,number));
 alert(results)
 
 
